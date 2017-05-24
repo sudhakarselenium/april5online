4fddb7c01e7d9f63c2861c585dd24edd67bbaf23
sudhakarselenium <me@qtpsudhakar.com>
2017-05-24 17:37:58 +0530

    demo updated

--- src/test/java/com/tst/Demo.java
+++ src/test/java/com/tst/Demo.java
@@ -5,6 +5,7 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("this is first change");
+		System.out.println("new modification");
 	}
 
 }
